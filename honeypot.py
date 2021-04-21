d4ba8a3b071d5cba891bb346f403f6aa41ce15b7
dylsugar <65335816+dylsugar@users.noreply.github.com>
2021-04-20 23:46:03 -1000

    Update honeypot.py

--- honeypot.py
+++ honeypot.py
@@ -1,78 +1,64 @@
-import socket
-our_log=open("Attackers_Data.txt","w") #Our text file to save attackers data in it
+#!/usr/bin/env python2.7
+import socket, sys, threading, thread
+import paramiko
 
-def ssh(msg="",listeners=2):
-    welcome="""Welcome to BackBox Linux 4.5 (GNU/Linux 4.2.0-30-generic i686)\n
- * Documentation:  http://www.backbox.org/\n\n
-The programs included with the BackBox/Ubuntu system are free software;
-the exact distribution terms for each program are described in the
-individual files in /usr/share/doc/*/copyright.\n
-BackBox/Ubuntu comes with ABSOLUTELY NO WARRANTY, to the extent
-permitted by applicable law.\n
-    """
-    s = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('', 22))#binding for the ssh port
-    print "\nSSH Honeypot ready(Waiting For Attackers)..\n"
-    s.listen(int(listeners))
-    stat=0
-    n=0
-    rqs=["http","HTTP/1.0","GET","bind","version","OPTIONS"]
-    while True:
-        n+=1
-        c,attacker= s.accept()
-        port=attacker[1]
-        ip=attacker[0]
-        c.send("login as: ")
-        login=c.recv(1024)
-        c.send(login+"@host's password: ")
-        a=c.recv(1024)
-        PROMPT = login+"@host:~$"
-        c.send(welcome)
-        ips.append(ip)
-        our_log.write("\n ["+str(n)+"] IP: "+str(ip)+"\tPort: "+str(port)+"\n")
-        print "\n ["+str(n)+"] IP: "+str(ip)+"\tPort: "+str(port)+"\n"
-        c.send(PROMPT)
-        data = c.recv(1024)
+#generate keys with 'ssh-keygen -t rsa -f server.key'
+HOST_KEY = paramiko.RSAKey(filename='server.key')
+SSH_PORT = 2222
+LOGFILE = 'logins.txt' #File to log the user:password combinations to
+LOGFILE_LOCK = threading.Lock()
 
-        for rq in rqs:
-            if rq in data.split(" ") or data.split(" ")=="" or data==" " :
-                our_log.write(" ["+str(ip)+"] is Scanning us With nmap looking for service info.!"+"\n")
-                print " ["+str(ip)+"] is Scanning us With nmap looking for service info.!"+"\n"
-                if ip in ips:c.close()
-                stat=1
-                break
+class SSHServerHandler (paramiko.ServerInterface):
+    def __init__(self):
+        self.event = threading.Event()
 
-        if data.split(" ")[0] == "id":
-            our_log.write(" ["+str(ip)+"][!]Command: "+str(data)+"\n")
-            print " ["+str(ip)+"][!]Command: "+str(data)+"\n"
-            c.send("\nuid=0(root) gid=0(root) groups=0(root)")
-            our_log.write("  ["+str(ip)+"]>Output: uid=0(root) gid=0(root) groups=0(root)\n")
-            print "  ["+str(ip)+"]>Output: uid=0(root) gid=0(root) groups=0(root)\n"
-            c.send(str(msg)+'\n')
-            stat=1
-            c.close()
+    def check_auth_password(self, username, password):
+        LOGFILE_LOCK.acquire()
+        try:
+            logfile_handle = open(LOGFILE,"a")
+            print("New login: " + username + ":" + password)
+            logfile_handle.write(username + ":" + password + "\n")
+            logfile_handle.close()
+        finally:
+            LOGFILE_LOCK.release()
+        return paramiko.AUTH_FAILED
 
-        elif data.split(" ")[0] == "uname":
-            our_log.write(" ["+str(ip)+"]!]Command: "+str(data)+"\n")
-            print " ["+str(ip)+"][!]Command: "+str(data)+"\n"
-            c.send("\nLinux f001 3.13.3-7-high-octane-fueled #3000-LPG SMPx4 Fri Jun 31 25:24:23 UTC 2200 x86_64 x64_86 x13_37 GNU/Linux")
-            our_log.write("  ["+str(ip)+"]>Output: Linux f001 3.13.3-7-high-octane-fueled #3000-LPG SMPx4 Fri Jun 31 25:24:23 UTC 2200 x86_64 x64_86 x13_37 GNU/Linux\n")
-            print "  ["+str(ip)+"]>Output: Linux f001 3.13.3-7-high-octane-fueled #3000-LPG SMPx4 Fri Jun 31 25:24:23 UTC 2200 x86_64 x64_86 x13_37 GNU/Linux\n"
-            c.send(str(msg)+'\n')
-            stat=1
-            c.close()
 
-        elif stat==0:
-            our_log.write("\t[!]Command: "+str(data)+"\n")
-            print " ["+str(ip)+"][!]Command: "+str(data)+"\n"
-            c.send("\n"+str(data.split(" ")[0]) + ": command not found")
-            our_log.write("   ["+str(ip)+"]>Output: "+ data.split(" ")[0] + ": command not found\n")
-            print "   ["+str(ip)+"]>Output: "+ data.split(" ")[0] + ": command not found\n"
-            c.send(str(msg)+'\n')
-            c.close()
-        our_log.write("="*10)
-        print "="*10
+    def get_allowed_auths(self, username):
+        return 'password'
 
-    our_log.close()
+def handleConnection(client):
+    transport = paramiko.Transport(client)
+    transport.add_server_key(HOST_KEY)
 
-ssh()
+    server_handler = SSHServerHandler()
+
+    transport.start_server(server=server_handler)
+
+    channel = transport.accept(1)
+    if not channel is None:
+        channel.close()
+
+def main():
+    try:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind(('', SSH_PORT))
+        server_socket.listen(100)
+
+        paramiko.util.log_to_file ('paramiko.log') 
+
+        while(True):
+            try:
+                client_socket, client_addr = server_socket.accept()
+                thread.start_new_thread(handleConnection,(client_socket,))
+            except Exception as e:
+                print("ERROR: Client handling")
+                print(e)
+
+    except Exception as e:
+        print("ERROR: Failed to create socket")
+        print(e)
+        sys.exit(1)
+
+main()
