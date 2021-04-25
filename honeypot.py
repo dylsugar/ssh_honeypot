dbf9d79c2d5b19bce443a882e87ec21da379a32b
dylsugar <dylansuga@gmail.com>
2021-04-25 02:06:26 -0400

    almost done push

--- honeypot.py
+++ honeypot.py
@@ -44,6 +44,8 @@ class Server(paramiko.ServerInterface):
         self.event = threading.Event()
         t = threading.Thread(target=self.check_channel_shell_request,args=(self.event,))
         t.start()
+        self.uname = " "
+        self.count = 0
 
     def check_channel_request(self, kind, chanid):
         if kind == "session":
@@ -51,16 +53,8 @@ class Server(paramiko.ServerInterface):
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 
     def check_auth_password(self, username, password):
-        f = open("usernames.txt",'r')
-        file_list = f.readlines()
-        user_list = []
-        for x in file_list:
-            x = x.strip()
-            user_list.append(x)
-        if username in user_list:
-            USERNAME = username
-            return paramiko.AUTH_SUCCESSFUL
-        return paramiko.AUTH_FAILED
+        self.uname = username
+        return paramiko.AUTH_SUCCESSFUL
 
     #def check_auth_publickey(self, username, key):
     #    print("Auth attempt with key: " + u(hexlify(key.get_fingerprint())))
@@ -113,98 +107,126 @@ class Server(paramiko.ServerInterface):
 
 DoGSSAPIKeyExchange = True
 
+flag = 0
+user_attempt = {}
+while flag == 0:
 # now connect
-try:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(("", 22))
-except Exception as e:
-    print("*** Bind failed: " + str(e))
-    traceback.print_exc()
-    sys.exit(1)
-
-HOST = socket.gethostbyname(socket.gethostname())
-PORT = 22
-USERNAME = 'Amir71'
-try:
-    sock.listen(100)
-    print("Listening for connection ...")
-    client, addr = sock.accept()
-except Exception as e:
-    print("*** Listen/accept failed: " + str(e))
-    traceback.print_exc()
-    sys.exit(1)
-
-print("Got a connection!")
-
-try:
-    t = paramiko.Transport(client)
-    t.set_gss_host(socket.getfqdn(""))
-    t.load_server_moduli()
-    t.add_server_key(host_key)
-    server = Server()
-    print("After paramiko...")
     try:
-        t.start_server(server=server)
-    except paramiko.SSHException:
-        print("*** SSH negotiation failed.")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("", 22))
+    except Exception as e:
+        print("*** Bind failed: " + str(e))
+        traceback.print_exc()
         sys.exit(1)
-    # wait for auth
-    chan = t.accept(20)
-    if chan is None:
-        print("*** No channel.")
-        sys.exit(1)
-    print("Authenticated!")
 
-    server.event.wait(10)
-    if not server.event.is_set():
-        print("*** Client never asked for a shell.")
-        sys.exit(1)
-    
-    
-    #print(chan.recv(1024))
-    chan.send("\r\n\r\n############## HW5 Server ###################\r\n\r\n")
-    print("Username: ",USERNAME)
-    print("Host: ",HOST)
-    print('Port: ',PORT)
-    print(type(chan))
-    chan.send(USERNAME+"@honeypot :/$ ")
-    command = ""
-    while True:    
-
-        uinput = chan.recv(1024)
-        chan.send(uinput)
-        command = command + uinput.decode()
-        if uinput.decode() == '\r':
-            #chan.send("\r\n")
-            p = subprocess.Popen(command.strip(), shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-            print(p.stdout)
-            stdout,stderr=p.communicate()
-            chan.sendall(stdout)
-            chan.sendall_stderr(stderr)
-            chan.send_exit_status(p.returncode)
-            print(stdout)
-            #if result == []:
-            #    print(p.stderr.readlines())    
-            #    chan.send(p.stderr.readlines())
-            #else:
-            #    print(result)
-            #    chan.send(result)
-            chan.send("\r\n")
-            command = ""
-            chan.send(USERNAME+"@honeypot :/$ ")
-        if uinput == "exiting":
-            break
-    #while True:
-    #    chan.send(USERNAME+"@honeypot:/$")
-    #    command = chan.recv(1024)
-    #chan.exec_command(command)
-    chan.close()
-except Exception as e:
-    print("*** Caught exception: " + str(e.__class__) + ": " + str(e))
-    traceback.print_exc()
+    HOST = socket.gethostbyname(socket.gethostname())
     try:
-        t.close()
-    except:
-        pass
-    sys.exit(1)
+        sock.listen(100)
+        print("\n\n*************************************************************\n\n")
+        print("\n\nThe Homework 5 SSH Honeypot Server is booting........\n\n")
+        print("Server is Listening for connection ...\n\n")
+        client, addr = sock.accept()
+    except Exception as e:
+        print("*** Listen/accept failed: " + str(e))
+        traceback.print_exc()
+        sys.exit(1)
+
+    print("Got a connection!\n\n")
+
+    hey = True
+    if hey:
+        t = paramiko.Transport(client)
+        t.set_gss_host(socket.getfqdn(""))
+        t.load_server_moduli()
+        t.add_server_key(host_key)
+        server = Server()
+        f = open("usernames.txt",'r')
+        raw_output = f.readlines()
+        user_list = []
+        for x in raw_output:
+            x = x.strip()
+            user_list.append(x)
+
+        try:
+            t.start_server(server=server)
+        except paramiko.SSHException:
+            print("*** SSH negotiation failed.")
+            sys.exit(1)
+
+        print("Server has been chosen\n\n")
+        
+        print("Press 'Enter' for Password\n\n")
+        # wait for auth
+        chan = t.accept(20)
+        if chan is None:
+            print("*** No channel.")
+            sys.exit(1)
+   
+        print("Channel has been open\n\n")
+        server.event.wait(10)
+        if not server.event.is_set():
+            print("*** Client never asked for a shell.")
+            sys.exit(1)
+       
+        if server.uname not in user_list:
+            print(server.uname,"not Authentic:\n\n")
+            if server.uname not in user_attempt:
+                user_attempt[server.uname]=0
+            else:
+                user_attempt[server.uname]+=1
+            if user_attempt[server.uname] < 6:
+                print(server.uname," try: ",user_attempt[server.uname],"\n\n")
+                chan.close()
+                sock.close()
+                print("Client connection channel has been closed\n\n")
+                continue
+            print("User ",server.uname," is not Authentic, but # Attempts > 5\n\n")
+        print("User",server.uname," is Authentic\n\n")
+
+        chan.send("\r\n\r\n######################################## HW5 Honeypot SSH Server ########################################\r\n\r\n")
+        chan.send("\n********************************************************")
+        chan.send("\r\n\r\nWelcome to the Hw5 SSH Server\r\n\r\n")
+        chan.send("\r\n\r\n---Type 'exit' to exit SSH Server\r\n\n")
+        chan.send("*********************************************************\r\n")
+        chan.send(server.uname+"@honeypot:/$ ")
+        command = ""
+        while True:    
+            uinput = chan.recv(1024)
+            chan.send(uinput)
+            command = command + uinput.decode()
+            if uinput.decode() == '\r':
+                command = command.strip()
+                chan.send("\r\n")
+                each_command = command.split(' ')
+                print(each_command)
+                currwodir = os.getcwd()
+                print(currwodir)
+                if len(each_command) > 1:
+                    directory = each_command[1].strip('/')
+                else:
+                    directory = each_command[0]
+
+                if 'cd' in each_command:
+                    try:
+                        os.chdir(os.path.abspath(directory))
+                        cwd = os.getcwd()
+                        print("CWD: ",cwd)
+                    except:
+                        chan.send("-bash: cd: "+directory+": No such file or directory")
+                else:
+                    p = subprocess.Popen(command, shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+                    print(p.stdout)
+                    if command == "exit":
+                        break
+                    stdout,stderr=p.communicate()
+                    print(stdout)
+                    output = ((stdout.decode()).replace('\n','   ')).encode()
+                    chan.sendall(output)
+                    chan.sendall_stderr(stderr)
+                    chan.send_exit_status(p.returncode)
+                    print(stdout)
+                chan.send("\r\n")
+                command = ""
+                chan.send(server.uname+"@honeypot:/$ ")
+        chan.close()
